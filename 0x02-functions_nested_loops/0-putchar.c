289c2fce566a4b3cc6211cde2db47e44eecbff0e
IsaacAnsong <ikwadwoansong@gmail.com>
2022-10-19 05:35:57 -0700

    update commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * main - program that prints Holberton.
@@ -8,14 +8,16 @@
 
 int main(void)
 {
-	int n = 0;
-	char str_hbt[9] = "Holberton";
-
-	while (n < 9)
-	{
-		_putchar(str_hbt[n]);
-		n += 1;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
+
 	return (0);
 }
