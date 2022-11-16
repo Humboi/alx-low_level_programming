3f6086b760059527902bfa02bcfa776ee76e71fd
IsaacAnsong <ikwadwoansong@gmail.com>
2022-11-16 05:04:58 -0800

    Repo commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,11 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOG
+#define DOG
+int _putchar(char c);
 /**
- * struct dog - list dates about pet
- * @name: pet name
- * @age: pet age
- * @owner: pet owner name
+ * struct dog - dog struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  */
 struct dog
 {
@@ -12,12 +13,10 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/**
- * dog - rename strucut with typedef
- */
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif /* DOG_H */
+int _strlen(char *s);
+#endif
